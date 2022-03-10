fc75e286fac40b6201460c0d62094ea6c91baede
davidkernfhnw <97611154+davidkernfhnw@users.noreply.github.com>
2022-03-10 21:00:55 +0100

    vue router

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar fixed app>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-title>
+        <img
+          src=".//assets/geiger_logo.png"
+          alt="logo"
+          class="logo pr-4"
+          width="120"
+        />
+      </v-app-bar-title>
+      <v-app-bar-title>Tools Installation</v-app-bar-title>
+    </v-app-bar>
+    <v-main>
+      
+        <router-view></router-view>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+
+  components: {},
+
+  data: () => ({
+    //
+  }),
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.logo {
+  padding-top: 15px;
 }
 </style>
