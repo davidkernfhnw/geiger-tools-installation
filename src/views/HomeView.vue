6e01462387a5c30c70374dc6f8df596f64e7974e
davidkernfhnw <97611154+davidkernfhnw@users.noreply.github.com>
2022-03-11 15:46:47 +0100

    test download

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,76 @@
 <template>
-<v-container fluid class="pa-10">
-  <v-row class="d-flex justify-center">
-    <ToolsItem v-for="i in 9" v-bind:key=i ></ToolsItem>
-  </v-row>
-</v-container>
+  <v-container fluid >
+    <v-row class="d-flex justify-center">
+      <ToolsItem
+        v-for="tool in tools"
+        v-bind:key="tool"
+        :title="tool.name"
+        :android="tool.android"
+        :apple="tool.apple"
+        :microsoft="tool.microsof"
+        :linux="tool.linux"
+        :image="tool.image"
+        btnText="Get Tool"
+       
+      ></ToolsItem>
+    </v-row>
+
+  </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import ToolsItem from '../components/ToolsItem.vue'
+import ToolsItem from "../components/ToolsItem.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    ToolsItem
-  }
-}
+    ToolsItem,
+  },
+  data: () => ({
+    tools: [
+      //image are located in src/assets
+      // only provide file name instead of full path
+      {
+        name: "Tool Name",
+        android: true,
+        microsoft: false,
+        apple: false,
+        linux: false,
+        image: "tool.png",
+        
+      },
+      {
+        name: "Tool Name2",
+        android: false,
+        microsoft: true,
+        apple: false,
+        linux: false,
+        image: "tool.png",
+      },
+      {
+        name: "Tool Name3",
+        android: true,
+        microsoft: false,
+        apple: true,
+        linux: false,
+        image: "tool.png",
+      },
+      {
+        name: "Tool Name4",
+        android: true,
+        microsoft: false,
+        apple: false,
+        linux: true,
+        image: "tool.png",
+      },
+    ],
+  }),
+  methods: {
+    
+   
+
+
+  },
+};
 </script>
